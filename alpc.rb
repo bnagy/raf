c5ab238a7200ace3f05dd934808698996d5c84d3
ben <git@ben.iagu.net>
2014-09-18 13:24:24 +1100

    refactor and bugfix

---
+++ alpc.rb
@@ -0,0 +1,178 @@
+module ALPC
+
+  def local_kernel_target?
+    @p_target_class      ||= p_ulong
+    @p_target_qualifier  ||= p_ulong
+    retval = self.debug_client.DebugControl.GetDebuggeeType( @p_target_class, @p_target_qualifier )
+    self.raise_errorcode( retval, __method__ ) unless retval.zero? # S_OK
+
+    @p_target_class.read_int == DebugControl::DEBUG_CLASS_KERNEL &&
+      @p_target_qualifier.read_int == DebugControl::DEBUG_KERNEL_LOCAL
+  end
+
+  def get_processes_k
+    raise "No local kernel target" unless local_kernel_target?
+    # Get a list of processes from the kernel side
+    processes = self.execute("!process 0 0")
+    processes.sub! "**** NT ACTIVE PROCESS DUMP ****\n", ''
+    # PROCESS fffffa8030cc6040
+    #   SessionID: none  Cid: 0004    Peb: 00000000  ParentCid: 0000
+    #   DirBase: 00187000  ObjectTable: fffff8a0000017e0  HandleCount: 496.
+    #   Image: System
+    #
+    chunks = processes.split("\n\n")
+    # Slurp them into a hash
+    p_ary = chunks.map {|chunk| Hash[*chunk.delete(":\n").squeeze(' ').split(' ')]}
+    # clean up
+    final = {}
+    p_ary.each {|p|
+      p_obj = p['PROCESS'].to_i(16)
+      final[p_obj] = {
+        object:       p_obj,
+        session_id:   (Integer(p['SessionID']) rescue 'none'),
+        pid:          p['Cid'].to_i(16),
+        peb:          p['Peb'].to_i(16),
+        ppid:         p['ParentCid'].to_i(16),
+        dir_base:     p['DirBase'].to_i(16),
+        object_table: p['ObjectTable'].to_i(16),
+        handle_count: p['HandleCount'][0..-2].to_i(10),
+        image:        p['Image']
+      }
+    }
+    final
+  end
+
+  def get_alpc_connections proc_obj
+    raise "No local kernel target" unless local_kernel_target?
+    # Get all the ALPC ports this process is connected to
+    lpp = self.execute("!alpc /lpp 0n#{proc_obj}").lines.map(&:chomp)
+    #
+    #  Ports created by the process fffffa80353c3060:
+    #
+    #  fffffa8032129590('OLE16E02A5AAD974222920005479E7C') 0, 1 connections
+    #    fffffa8033c6fb80 0 -> fffffa803262f900 0 fffffa80321ac580('svchost.exe')
+    #
+    # Ports the process fffffa80353c3060 is connected to:
+    #
+    #  fffffa8035261e60 0 -> fffffa8032059e60('ApiPort') 0 fffffa8032e68b30('csrss.exe')
+    #  fffffa80353ce070 0 -> fffffa8033c2a6b0('ThemeApiPort') 0 fffffa8032fd2b30('svchost.exe')
+    #  fffffa80365bb8c0 0 -> fffffa803211a5a0('lsasspirpc') 0 fffffa80320a3440('lsass.exe')
+    #  fffffa80365b2e60 0 -> fffffa8032174cf0('ntsvcs') 19 fffffa803207fb30('services.exe')
+    lpp.shift until lpp.first =~ /Ports the process .* is connected to/
+    lpp.shift 2
+
+    connections = {}
+    lpp.each {|l|
+      break if l.empty?
+      src, dst, dest_proc = l.scan(/ffff[a-f0-9]{8,}/)
+      connections[src.to_i(16)] = {
+        port: dst.to_i(16),
+        name: get_absolute_port_name(dst.to_i(16)),
+        proc: dest_proc.to_i(16)
+      }
+    }
+    connections
+  end
+
+  def get_alpc_ports proc_obj
+    raise "No local kernel target" unless local_kernel_target?
+    # Get all the ALPC ports this process hosts
+    lpp = self.execute("!alpc /lpp 0n#{proc_obj}")
+    unless lpp =~ /Ports created by the process/
+      raise "Parsing failed. Perhaps #{proc_obj.to_s(16)} is not a process object?"
+    end
+    lpp = lpp.lines.map(&:chomp)
+    #
+    #  Ports created by the process fffffa80353c3060:
+    #
+    #  fffffa8032129590('OLE16E02A5AAD974222920005479E7C') 0, 1 connections
+    #    fffffa8033c6fb80 0 -> fffffa803262f900 0 fffffa80321ac580('svchost.exe')
+    #
+    # Ports the process fffffa80353c3060 is connected to:
+    #
+    #  fffffa8035261e60 0 -> fffffa8032059e60('ApiPort') 0 fffffa8032e68b30('csrss.exe')
+    #  fffffa80353ce070 0 -> fffffa8033c2a6b0('ThemeApiPort') 0 fffffa8032fd2b30('svchost.exe')
+    #  fffffa80365bb8c0 0 -> fffffa803211a5a0('lsasspirpc') 0 fffffa80320a3440('lsass.exe')
+    #  fffffa80365b2e60 0 -> fffffa8032174cf0('ntsvcs') 19 fffffa803207fb30('services.exe')
+    ports = {}
+    while line = lpp.shift
+      break if line =~ /Ports the process .* is connected to/
+      if line =~ /^ffff.*connections$/
+        obj = l.split('(').first
+        ports[obj.to_i(16)] = get_absolute_port_name obj
+      end
+    end
+    ports
+  end
+
+  def get_handles_k proc_obj
+    raise "No local kernel target" unless local_kernel_target?
+    # Map userland handle ids to kernel object ids - this is the slowest part,
+    # because we walk the whole handle list
+    handles = self.execute("!handle 0 1 0n#{Integer(proc_obj)}")
+    unless handles =~ /PROCESS/
+      raise "Parsing failed. Perhaps #{proc_obj.to_s(16)} is not a process object?"
+    end
+    # lkd> !handle 0 1 8d0
+    #
+    # Searching for Process with Cid == 8d0
+    # PROCESS fffffa803213e060
+    #     SessionId: 1  Cid: 08d0    Peb: 7fffffde000  ParentCid: 05c8
+    #     DirBase: 2547e000  ObjectTable: fffff8a002ebedc0  HandleCount: 285.
+    #     Image: notepad.exe
+    #
+    # Handle table at fffff8a002ebedc0 with 285 entries in use
+    #
+    # 0004: Object: fffff8a00305d520  GrantedAccess: 00000009
+    #
+    # 0008: Object: fffff8a005c7a120  GrantedAccess: 00000003
+    #
+    # 000c: Object: fffffa8032122f20  GrantedAccess: 00100020
+    chunks = handles.split("\n\n")
+    hids = {}
+    chunks.each {|chunk|
+      next unless chunk =~ /^[0-9a-f]+: /
+      hid, _, object, _ = chunk.split(/[ :]+/)
+      hids[object.to_i(16)] = hid.to_i(16)
+    }
+    # make it a two-way lookup, hids and object ids can't collide.
+    hids.update hids.invert
+    hids
+  end
+
+  def get_absolute_port_name port_obj
+    raise "No local kernel target" unless local_kernel_target?
+    # Follow each ALPC Port object back to the root of the Object Directory so we
+    # get the "absolute" ALPC Port name.
+    # Working backwards to the root, like this:
+    # lkd> !object fffffa80352ea740 3
+    #  Object: fffffa80352ea740  Type: (fffffa8030d11080) ALPC Port
+    #     ObjectHeader: fffffa80352ea710 (new version)
+    #     HandleCount: 1  PointerCount: 4
+    #     Directory Object: fffff8a000a4c450  Name: OLE8F8B8C095131496BB200263FA52C
+    # lkd> !object fffff8a000a4c450 3
+    # Object: fffff8a000a4c450  Type: (fffffa8030c64f30) Directory
+    #     ObjectHeader: fffff8a000a4c420 (new version)
+    #     HandleCount: 0  PointerCount: 72
+    #     Directory Object: fffff8a0000046c0  Name: RPC Control
+    # lkd> !object fffff8a0000046c0 3
+    # Object: fffff8a0000046c0  Type: (fffffa8030c64f30) Directory
+    #     ObjectHeader: fffff8a000004690 (new version)
+    #     HandleCount: 0  PointerCount: 44
+    #     Directory Object: 00000000  Name: \
+
+    name = []
+    next_obj = port_obj.to_s(16)
+    sane_depth = 5
+    loop do
+      lines = self.execute("!object #{next_obj} 3").lines.map(&:chomp)
+      toks = lines[3].split(' ',5)
+      break if toks.last == '\\' || name.length > sane_depth
+      name.unshift toks.last
+      next_obj = toks[2]
+    end
+
+    "\\#{name.join('\\')}"
+  end
+
+end
--- alpclive.rb
+++ alpclive.rb
@@ -4,6 +4,8 @@ require 'bindata'
 require 'hexdump'
 require 'csv'
 
+require 'alpc'
+
 include Buggery::Raw
 include Buggery::Structs
 
@@ -11,6 +13,7 @@ target = ARGV[0]
 fail "Usage: #{$0} <target pid>" unless Integer(target)
 
 debugger = Buggery::Debugger.new
+debugger.extend ALPC
 
 # Used to track per-thread which HID started the call to NtAlpcSendReceivePort
 last_hid = {}
@@ -20,12 +23,12 @@ last_hid = {}
 last_hidptr = {}
 
 # Connections that we're tracking
-conn = Hash.new {|h,k| h[k] = {name: nil, pid: nil, count: 0, last_count: 0}}
+live_conns = Hash.new {|h,k| h[k] = {name: nil, pid: nil, count: 0, last_count: 0}}
 conn_mtx = Mutex.new
 conn_q = Queue.new
 
 def refresh_pids
-  @pids = {}
+  @pids ||= {}
   CSV.parse(`tasklist /v /fo csv`, headers: true) {|row|
     @pids[row["PID"].to_i] = row
   }
@@ -56,7 +59,7 @@ Thread.new do
     begin
 
       hid, raw = output_q.pop
-      name = conn_mtx.synchronize { conn[hid][:name] }
+      name = conn_mtx.synchronize { live_conns[hid][:name] }
       # We'll also receive messages on our own OLE ports, or whatever other
       # ALPC ports we might have, but they're not interesting for this
       # purpose, so we didn't record their names in the kernel phase
@@ -66,8 +69,8 @@ Thread.new do
 
       if m.process.nonzero?
         conn_mtx.synchronize {
-          conn[hid][:count] += 1
-          conn[hid][:pid] = m.process
+          live_conns[hid][:count] += 1
+          live_conns[hid][:pid] = m.process
         }
       end
 
@@ -133,7 +136,15 @@ bp_proc = lambda {|args|
       hid = debugger.read_virtual( ptr, 8 ).unpack('Q').first
       conn_q.push "New connection: HID: #{hid} -> #{name}"
       conn_mtx.synchronize {
-        conn[hid][:name] = name
+        # FIXME live_conns should probably just be name -> conn
+        carry = 0
+        old_conns = live_conns.select {|hid,c| c[:name] == name}
+        old_conns.each {|old_hid,old_conn|
+          carry += old_conn[:count]
+          live_conns.delete old_hid
+        }
+        live_conns[hid][:name] = name
+        live_conns[hid][:count] = carry
       }
 
     end
@@ -154,133 +165,35 @@ debugger.attach_local_kernel
 debugger.wait_for_event
 
 # Get a list of processes from the kernel side
-processes = debugger.execute("!process 0 0")
-processes.sub! "**** NT ACTIVE PROCESS DUMP ****\n", ''
-# PROCESS fffffa8030cc6040
-#   SessionID: none  Cid: 0004    Peb: 00000000  ParentCid: 0000
-#   DirBase: 00187000  ObjectTable: fffff8a0000017e0  HandleCount: 496.
-#   Image: System
-#
-chunks = processes.split("\n\n")
-# Slurp them into a hash
-proc_entries = chunks.map {|chunk| Hash[*chunk.delete(":\n").squeeze(' ').split(' ')]}
-
-# Find our target process
-proc = proc_entries.find {|p| p['Cid'].to_i(16) == target.to_i}
-unless proc
+procs = debugger.get_processes_k
+target_proc = procs.find {|k,v| v[:pid] == target.to_i }.last
+unless target
   warn "Unable to find target #{target}"
   fail "Usage: #{$0} <target pid>"
 end
-
-# Get all the ALPC ports this process is connected to
-lpp = debugger.execute("!alpc /lpp #{proc["PROCESS"]}").lines.map(&:chomp)
-#
-#  Ports created by the process fffffa80353c3060:
-#
-#  fffffa8032129590('OLE16E02A5AAD974222920005479E7C') 0, 1 connections
-#    fffffa8033c6fb80 0 -> fffffa803262f900 0 fffffa80321ac580('svchost.exe')
-#
-# Ports the process fffffa80353c3060 is connected to:
-#
-#  fffffa8035261e60 0 -> fffffa8032059e60('ApiPort') 0 fffffa8032e68b30('csrss.exe')
-#  fffffa80353ce070 0 -> fffffa8033c2a6b0('ThemeApiPort') 0 fffffa8032fd2b30('svchost.exe')
-#  fffffa80365bb8c0 0 -> fffffa803211a5a0('lsasspirpc') 0 fffffa80320a3440('lsass.exe')
-#  fffffa80365b2e60 0 -> fffffa8032174cf0('ntsvcs') 19 fffffa803207fb30('services.exe')
-lpp.shift until lpp.first =~ /Ports the process .* is connected to/
-lpp.shift 2
-
-port_obj = {}
-lpp.each {|l|
-  break if l.empty?
-  src, dst, dest_proc = l.scan(/ffff[a-f0-9]{8,}/)
-  port_obj[src] = [dst, dest_proc]
-}
-
-# Map userland handle ids to kernel object ids - this is the slowest part,
-# because we walk the whole handle list
-handles = debugger.execute("!handle 0 1 #{proc['Cid']}")
-# lkd> !handle 0 1 8d0
-#
-# Searching for Process with Cid == 8d0
-# PROCESS fffffa803213e060
-#     SessionId: 1  Cid: 08d0    Peb: 7fffffde000  ParentCid: 05c8
-#     DirBase: 2547e000  ObjectTable: fffff8a002ebedc0  HandleCount: 285.
-#     Image: notepad.exe
-#
-# Handle table at fffff8a002ebedc0 with 285 entries in use
-#
-# 0004: Object: fffff8a00305d520  GrantedAccess: 00000009
-#
-# 0008: Object: fffff8a005c7a120  GrantedAccess: 00000003
-#
-# 000c: Object: fffffa8032122f20  GrantedAccess: 00100020
-chunks = handles.split("\n\n")
-hids = {}
-chunks.each {|chunk|
-  next unless chunk =~ /^[0-9a-f]+: /
-  hid, _, object, _ = chunk.split(/[ :]+/)
-  hids[object] = hid
-}
-# make it a two-way lookup, hids and object ids can't collide.
-hids.update hids.invert
-
-# Follow each ALPC Port object back to the root of the Object Directory so we
-# get the "absolute" ALPC Port name.
-# Working backwards to the root, like this:
-# lkd> !object fffffa80352ea740 3
-#  Object: fffffa80352ea740  Type: (fffffa8030d11080) ALPC Port
-#     ObjectHeader: fffffa80352ea710 (new version)
-#     HandleCount: 1  PointerCount: 4
-#     Directory Object: fffff8a000a4c450  Name: OLE8F8B8C095131496BB200263FA52C
-# lkd> !object fffff8a000a4c450 3
-# Object: fffff8a000a4c450  Type: (fffffa8030c64f30) Directory
-#     ObjectHeader: fffff8a000a4c420 (new version)
-#     HandleCount: 0  PointerCount: 72
-#     Directory Object: fffff8a0000046c0  Name: RPC Control
-# lkd> !object fffff8a0000046c0 3
-# Object: fffff8a0000046c0  Type: (fffffa8030c64f30) Directory
-#     ObjectHeader: fffff8a000004690 (new version)
-#     HandleCount: 0  PointerCount: 44
-#     Directory Object: 00000000  Name: \
-port_names = {}
-port_obj.each {|src,(dst,dest_proc)|
-
-  name = []
-  next_obj = dst
-  sane_depth = 5
-  loop do
-
-    lines = debugger.execute("!object #{next_obj} 3").lines.map(&:chomp)
-    toks = lines[3].split(' ',5)
-    break if toks.last == '\\' || name.length > sane_depth
-    name.unshift toks.last
-    next_obj = toks[2]
-
-  end
-
-  port_names[dst] = "\\#{name.join('\\')}"
-}
+# Existing ALPC connections
+conns = debugger.get_alpc_connections target_proc[:object]
+# Map of handle IDs to object IDs
+hids = debugger.get_handles_k target_proc[:object]
 
 puts "Existing external ALPC Port handles:"
-port_obj.each {|src,(dst,dst_proc_obj)|
+conns.each {|src,dst|
 
-  dst_proc = proc_entries.find{|p| p['PROCESS']==dst_proc_obj}
+  dst_proc_obj = procs[dst[:proc]]
+  hid = hids[src]
 
-  unless dst_proc
-    p dst_proc_obj
-    p proc_entries
+  unless dst_proc_obj
     fail "Unable to find dest process for port #{src} in kernel phase"
   end
-  fail "Unable to find #{src} in:\n#{hids}" unless hids[src]
+  fail "Unable to find #{src} in:\n#{hids}" unless hid
 
-  puts "HID: #{hids[src]} -> #{dst_proc['Image']} : #{port_names[dst]}"
+  puts "HID: #{hid} -> #{dst_proc_obj[:image]} : #{dst[:name]}"
 
-  hid = hids[src].to_i 16
-  conn[hid][:name] = port_names[dst]
-  conn[hid][:pid] = dst_proc['Cid'].to_i 16
+  live_conns[hid][:name] = dst[:name]
+  live_conns[hid][:pid] = dst_proc_obj[:pid]
 }
 
-puts "trying to detach"
+puts "Detaching from local kernel."
 
 # Now we have a map of userland handles to kernel objects, and we have details
 # on all the kernel objects that back handles to ALPC ports in other
@@ -331,14 +244,14 @@ Thread.new do
       sleep 5
 
       # drain the notification queue of recent connections
-      puts
+      puts unless conn_q.empty?
       until conn_q.empty?
         puts conn_q.pop
       end
       puts
 
       conn_mtx.synchronize {
-        conn.each {|hid, c|
+        live_conns.each {|hid, c|
 
           next unless c[:count] > 0 || c[:name]
 
